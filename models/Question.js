d3331f7182d0b0e6356fe46c019867ef3c0e4725
Max han <mwellh84@gmail.com>
2017-11-19 16:14:09 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -20,7 +20,7 @@ class Question{
 
   insert() {
     const self = this
-    const sql = `INSERT INTO users (content) VALUES (?)`
+    const sql = `INSERT INTO questions (content) VALUES (?)`
     console.log(`we are inserting ${self.content} into the Database`)
 
     return new Promise(function(resolve){
